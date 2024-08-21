7cb47d7fe4a0a343d5cf37a5998a23a49be017d2
ak <amk251995@gmail.com>
2024-08-21 23:16:24 +0530

    Added File

--- PracticeQuestions/HackerRank/ArithmeticOperators.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter First Number "))
-b = int(input("Enter First Number "))
-
-
-print(a + b)
-print(a - b)
-print(a * b)
--- PracticeQuestions/HackerRank/Sequence.py
+++
@@ -1,5 +0,0 @@
-from collections.abc import Sequence
-
-n=3
-for i in range(1,n+1):
-    print(i, end='')
